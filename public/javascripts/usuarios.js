718c47ae7ef8148b7721c0531a92f86d51a6905c
David Castelblanco <david.castelblanco@hotmail.com>
2015-09-03 16:21:11 -0500

    Se movio el código componente Usuarios a un archivo aparte.

---
+++ public/javascripts/usuarios.js
@@ -0,0 +1,15 @@
+
+var app = app || {};
+
+(function(){
+
+  app.Usuarios = React.createClass({
+    render: function() {
+      return (
+        <section className="usuarios">
+        </section>
+      );
+    }
+  });
+
+})();
