5118a0aed6e6541e394a8930d44f6478fba525ad
David Castelblanco <david.castelblanco@hotmail.com>
2015-09-03 17:43:58 -0500

    Se refactorizo el archivo view.js, para que solo contenga el componente gráfico Vista

--- public/javascripts/view.js
+++ public/javascripts/view.js
@@ -1,92 +1,33 @@
-var arregloDias = [
-  'Lunes',
-  'Martes',
-  'Miércoles',
-  'Jueves',
-  'Viernes'
-]
 
-var Vista = React.createClass({
-  render: function(){
-    return (
-      <section className="vista">
-        <Usuarios />
-        <Calendario dias={this.props.dias}/>
-      </section>
-    );
-  }
-});
+var app = app || {};
 
-var Calendario = React.createClass({
-  render: function() {
-    var dias = this.props.dias.map(function(d){
+(function(){
+
+  var Usuarios = app.Usuarios;
+  var Calendario = app.Calendario;
+
+  var arregloDias = [
+    'Lunes',
+    'Martes',
+    'Miércoles',
+    'Jueves',
+    'Viernes'
+  ];
+
+  app.Vista = React.createClass({
+    render: function(){
       return (
-        <CalendarioDia name={d} />
-      )
-    });
+        <section className="vista">
+          <Usuarios />
+          <Calendario dias={this.props.dias}/>
+        </section>
+      );
+    }
+  });
 
-    return (
-      <section className="calendario">
-        {dias}
-      </section>
-    );
-  }
-});
+  React.render(
+    <app.Vista dias={arregloDias}/>,
+    document.getElementById('contenido')
+  );
 
-var CalendarioDia = React.createClass({
-  render: function() {
-    return (
-      <section className="calendario__dia">
-        <CalendarioLabel text={this.props.name} />
-        <CalendarioBloque />
-        <CalendarioBloque />
-        <CalendarioBloque />
-        <CalendarioBloque />
-        <CalendarioBloque />
-        <CalendarioBloque />
-      </section>
-    );
-  }
-});
-
-var CalendarioBloque = React.createClass({
-  getInitialState: function(){
-    return {value:0, color:{red : 255, green: 255, blue: 255}};
-  },
-  aumentar: function(e) {
-    var temp_value = this.state.value + 1;
-    var temp_red = this.state.color.red - 20;
-    var temp_blue = this.state.color.blue - 20;
-    this.setState({value: temp_value, color:{red:temp_red,green:255,blue:temp_blue}});
-  },
-  render: function(){
-    var color = this.state.color.red + ',' + this.state.color.green + ',' + this.state.color.blue;
-    return (
-      <div className="calendario__bloque"
-           style={{'backgroundColor': 'rgb('+ color + ')'}}
-           onClick={this.aumentar}>
-      </div>
-    );
-  }
-});
-
-var CalendarioLabel = React.createClass({
-  render: function() {
-    return (
-      <div className="calendario__label">
-        {this.props.text}
-      </div>
-    );
-  }
-});
-
-var Usuarios = React.createClass({
-  render: function() {
-    return (
-      <section className="usuarios">
-      </section>
-    );
-  }
-});
-
-React.render(<Vista dias={arregloDias}/>, document.getElementById('contenido'));
+})();
