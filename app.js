256edda90bb7be8c05739b67868890dde93323bf
David Castelblanco <david.castelblanco@hotmail.com>
2015-09-14 19:53:53 -0500

    Se implemento un render básico del lado del servidor, con el template de index.ejs

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 
+require('node-jsx').install();
+
 var express = require('express');
 var http = require('http');
+
+var React = require('react/addons');
+var View = require('./public/javascripts/components/Vista');
+
+var Vista = React.createFactory(View);
 
 var app = express();
 
@@ -8,8 +15,31 @@ var PORT = 3000;
 
 app.use('/', express.static(__dirname+'/public'));
 
-app.get('/', function(req,res) {
-  res.sendFile(__dirname+'/public/index.html');
+app.set('views','./views');
+app.set('view engine', 'ejs');
+
+var colors = [
+  '#FFFFFF',
+  '#E8F5E9',
+  '#C8E6C9',
+  '#A5D6A7',
+  '#81C784',
+  '#66BB6A',
+  '#4CAF50',
+  '#43A047',
+  '#388E3C',
+  '#2E7D32',
+  '#1B5E20'
+];
+
+app.get('/home', function(req,res) {
+  // res.sendFile(__dirname+'/public/index.html');
+  var text = React.renderToString(Vista({
+    colores: colors
+  }));
+  res.render('index',{
+    contenido : text
+  });
 });
 
 
