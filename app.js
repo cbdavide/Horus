71e01066ee43cc1eda36e71113b25dcd6182fa30
David Castelblanco <david.castelblanco@hotmail.com>
2015-10-11 17:18:57 -0500

    Se creo el modulo SocketIO, que se encarga de la perte del real time

--- app.js
+++ app.js
@@ -1,38 +1,13 @@
 
 var http = require('http');
 var expressServer = require('./app/expressServer');
-var io = require('socket.io');
+var SocketIO = require('./app/SocketIO');
 
 var PORT = 3000;
 
 var server = http.createServer(expressServer.getServer());
+var io = new SocketIO(server);
 
-var sio = io(server);
-
-
-sio.on('connection', function(socket) {
-  console.log('Usuario conectado!');
-
-  socket.on('disconnect', function() {
-    console.log('Usuario desconectado :x');
-  });
-
-  socket.on('calendarConnection', function(calendarID) {
-    socket.join(calendarID.toString());
-    console.log('Conectado a ' + calendarID);
-  })
-
-  socket.on('calendarDisconnection', function(calendarID) {
-    socket.leave(calendarID.toString());
-    console.log('Desconectado de ' + calendarID);
-  })
-
-  socket.on('voto', function(data) {
-    console.log('Se ha registrado un voto para ' + data.bloqueID);
-    socket.broadcast.to(data.calendarID.toString()).emit('grabarVoto', data.bloqueID);
-  });
-
-});
 
 server.listen(PORT, function() {
   console.log('Servidor corriedno en el puerto '+PORT);
