30bd84211193c43b8c19900202315b41eb6d7342
David Castelblanco B <david.castelblanco@hotmail.com>
2016-05-27 16:58:21 -0500

    Installed bcrypt to hash passwords, refactored the tab problem of the express server, I load custom middlewares dynamucally now.

--- app/expressServer.js
+++ app/expressServer.js
@@ -1,32 +1,33 @@
-
+'use strict';
 var express = require('express');
 var bodyParser = require('body-parser');
+var middlewares = require('./middleware');
 var routers = require('./routers');
-
 
 var expressServer = (function() {
 
-  var server = express();
+    var server = express();
 
-  server.use('/', express.static(__dirname + '/public'));
-  server.use(bodyParser.urlencoded({extended: true}));
-  server.use(bodyParser.json());
+    server.use('/', express.static(__dirname + '/public'));
+    server.use(bodyParser.urlencoded({extended: true}));
+    server.use(bodyParser.json());
 
-  server.set('view engine', 'ejs');
-  server.set('views', './app/view');
+    server.set('view engine', 'ejs');
+    server.set('views', './app/view');
 
-  for(var i in routers) {
+    //Loading the middlewares
+    for(let middle of middlewares)
+        server[middle.method](middle.path, middle.middleware);
 
-    var router = routers[i];
-    server[router.method](router.path, router.callback);
+    //Loading the routers
+    for(let router of routers)
+        server[router.method](router.path, router.callback);
 
-  }
-
-  return {
-    getServer: function() {
-      return server;
+    return {
+        getServer: function() {
+            return server;
+        }
     }
-  }
 
 })();
 
